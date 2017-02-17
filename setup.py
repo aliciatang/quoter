a86f0c42992c02c3dc94dee49f12236b1732cbff
Alicia Tang <alicia.x.tang@gmail.com>
2017-02-17 08:08:26 -0800

    add setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='quoter',
+      version='0.0.1',
+      description='Quote stock price by ticker',
+      author='Alicia Tang',
+      author_email='alicia.x.tang@gmail.com',
+      url='https://github.com/aliciatang/quoter',
+      packages=['quoter'],
+     )
